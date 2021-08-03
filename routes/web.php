b4ca37bb4ef0663d065994663c3d1c396663624f
llf <llf@qq.com>
2021-08-03 23:06:17 +0800

    收货地址列表

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,8 @@ Route::get('/', 'PagesController@root')->name('root');
 
 Auth::routes(['verify' => true]);
 
+
+Route::group(['middleware'=>['auth','verified']],function(){
+    //收货地址
+    Route::get('user_addresses','UserAddressesController@index')->name('user_addresses.index');
+});

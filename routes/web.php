869776ceae3146a92bef6754a6e1b8dfa314d5ca
llf <llf@qq.com>
2021-08-03 23:54:41 +0800

    修改收货地址

--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,8 @@ Route::group(['middleware'=>['auth','verified']],function(){
     //新增收货地址
     Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
     Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
+
+    //修改地址信息
+    Route::get('user_addresses/{user_address}', 'UserAddressesController@edit')->name('user_addresses.edit');
+    Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
 });

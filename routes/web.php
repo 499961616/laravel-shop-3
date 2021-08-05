59d3ddc286fa5feb498cca17de35a973b8cb7da5
llf <llf@qq.com>
2021-08-05 22:49:21 +0800

    购物车页面

--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,10 @@ Route::group(['middleware'=>['auth','verified']],function(){
     Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
     //添加购物车
     Route::post('cart', 'CartController@add')->name('cart.add');
+    //查看购物车
+    Route::get('cart', 'CartController@index')->name('cart.index');
+//    购物车删除
+    Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
 });
 
 
